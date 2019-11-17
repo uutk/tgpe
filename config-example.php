cd3f93826470261f99fba406d58e8470855b49fe
SOORAJ S R <rsoorajs@gmail.com>
2019-11-17 23:26:53 +0530

    Update config-example.php

--- config-example.php
+++ config-example.php
@@ -1,9 +1,9 @@
 <?php
 define('TG_ADMINS', [
-	109780439  # Sean
+	687143857  # Sean
 ]);
 
 define('HTTP_API_TOKENS', [
 	'guest' => 'guest',
-	'Base64TokenOrAnySecretStringOnlyMeKnows' => 'admin'
+	'917948076:AAFzA5UrCBSwErXCYN3Ph08s7JmSDCi413Y' => 'admin'
 ]);
